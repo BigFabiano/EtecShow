a821d6005bae286281faea64ecc494010ecd46c7
FabianoEtec <nbscbs@gmail.com>
2021-11-07 15:14:31 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 const handleSearch = async (event) => {
   event.preventDefault();
-
-  // implemente a consulta a partir daqui
-
-  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
-
-  //// Elementos de leiaute importantes:
-
-  //  #message: use para exibir mensagens aos usuário, por exemplo:
-
+  //exibir mensagem de carregamento
   const message = document.querySelector('#message');
-  message.innerHTML = 'exercício ainda não resolvido.';
+  message.innerHTML = 'buscando...';
 
-  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
-  // <li>
-  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
-  //   <span class="show-name">Lost</span>
-  // </li>
+  // limpa lista de programas
+  const listaDeProgramas = document.querySelector('#shows');
+  listaDeProgramas.innerHTML = '';
+  //obter o texto digitado pelo usuario
+  const caixadeBusca = document.querySelector('#query');
+  const textoAserBuscado = caixadeBusca.value;
+  //formar a URL de consulta
+  const url = `https://api.tvmaze.com/search/shows?q=${textoAserBuscado}`;
+  //Realizar a consulta na API através Fetch API de forma assicrona
+  const resposta = await fetch(url); // aqui diz: acessa pra mim essa url, mais vamos gravar isso em uma variavel (await = aguarrdar), tem que esta dentro de uma função async
+  const programas = await resposta.json(); // esse objeto pega o response que vem da resposta e converte em JavaScript
+  //finaliza o procedimento caso nao haja nenhum resultado
+  if (programas.length === 0) {
+    message.innerHTML = 'nenhum resultado encontrado';
+    return; //limpar função
+  }
+  //limpar mensagem
+  message.innerHTML = '';
+  //interar pelos programas
+  programas.forEach((programa) => {
+    //obter os dados
+    const titulo = programa?.show?.name || ''; // ? para evitar error se achar erro mostra null, caso seja null coloca || (ou) para deixar vazio
+    const imagem = programa?.show?.image?.medium || '';
+
+    //Inserir os programas na lista de resultados
+
+    listaDeProgramas.insertAdjacentHTML(
+      'beforeend',
+      `
+    <li>
+    <img class="poster" src="${imagem}">
+    <span class="show-name">${titulo}</span>
+    </li>
+    `
+    );
+  }); //lista chamado programas(array ou arranjos)precisar de uma ero function
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   document
-    .querySelector('#search-form')
-    .addEventListener('submit', handleSearch);
+    .querySelector('#search-form') //buscando o formulario
+    .addEventListener('submit', handleSearch); //e aplicando o evento submite
 });
